1b485d2df485821856450584d5677dccf8e5d0df
Denny Kurniawan <dennykurniawanjr@gmail.com>
2024-04-25 13:11:18 +0700

    Update transaction_type values in create_transactions.sql

--- database/sql_migrations/3_create_transactions.sql
+++ database/sql_migrations/3_create_transactions.sql
@@ -4,7 +4,7 @@
 CREATE TABLE transactions (
   transaction_id SERIAL PRIMARY KEY,
   account_number VARCHAR(10) REFERENCES accounts(account_number) NOT NULL,
-  transaction_type VARCHAR(20) NOT NULL,  -- Deposit, Withdrawal, Transfer
+  transaction_type VARCHAR(20) NOT NULL,  -- deposit, withdrawal, send, receive
   amount DECIMAL(10,2) NOT NULL,
   description VARCHAR(255),
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
